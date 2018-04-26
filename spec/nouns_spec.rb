cfaf275e091c1cad6236add7843eb2d079465230
Zane <zane.a.sandin@uscis.dhs.gov>
2018-04-26 13:32:14 -0400

    Adds lets into context for spec

--- spec/nouns_spec.rb
+++ spec/nouns_spec.rb
@@ -1,6 +1,8 @@
-require_relative '../turkish_noun.rb' 
+require_relative '../turkish_noun.rb'
+require_relative 'turkish_nouns_context.rb'
 
 describe 'Turkish Nouns' do
+  include_context 'Turkish Nouns'
   context "Initialization" do
     it "downcases the noun given as input" do
       expect(TurkishNoun.new("ARABA").noun).to eq("araba")
@@ -8,15 +10,6 @@ describe 'Turkish Nouns' do
   end
 
   context "Plurals" do
-  	let(:araba) { TurkishNoun.new("araba") }
-  	let(:kapı) { TurkishNoun.new("kapı") }
-  	let(:top) { TurkishNoun.new("top") }
-  	let(:soru) { TurkishNoun.new("soru") }
-  	let(:kalem) { TurkishNoun.new("kalem") }
-  	let(:cami) { TurkishNoun.new("cami") }
-  	let(:göz) { TurkishNoun.new("göz") }
-  	let(:türk) { TurkishNoun.new("türk") }
-
     it "returns the plural form of a noun based on the last vowel" do
       expect(araba.plural).to eq("arabalar")
       expect(kapı.plural).to eq("kapılar")
@@ -27,6 +20,10 @@ describe 'Turkish Nouns' do
       expect(göz.plural).to eq("gözler")
       expect(türk.plural).to eq("türkler")
     end
+  end
+
+  context "Cases" do
+
   end
 end
 
---
+++ spec/turkish_nouns_context.rb
@@ -0,0 +1,10 @@
+RSpec.shared_context "Turkish Nouns" do
+  let(:araba) { TurkishNoun.new("araba") }
+  let(:kapı) { TurkishNoun.new("kapı") }
+  let(:top) { TurkishNoun.new("top") }
+  let(:soru) { TurkishNoun.new("soru") }
+  let(:kalem) { TurkishNoun.new("kalem") }
+  let(:cami) { TurkishNoun.new("cami") }
+  let(:göz) { TurkishNoun.new("göz") }
+  let(:türk) { TurkishNoun.new("türk") }
+end
