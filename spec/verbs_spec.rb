710a3312357b621068b435f1ce814cdfe2f15853
Maxx Alexander <creepinjezus15@gmail.com>
2018-05-08 22:11:29 -0400

    Created method for taking in turkish input downcasing and dropping the verbs suffix

--- spec/verbs_spec.rb
+++ spec/verbs_spec.rb
@@ -4,11 +4,11 @@ require_relative 'turkish_verbs_context.rb'
 describe 'Turkish Verbs' do
   include_context 'Turkish Verbs'
   context "Initialization" do
-    xit "should downcase the verb given as input" do
+    it "should downcase the verb given as input" do
       expect(TurkishVerb.new("YAPMAK").verb).to eq("yapmak")
     end
 
-    xit "should return the stem of the verb as an instance variable" do
+    it "should return the stem of the verb as an instance variable" do
       expect(TurkishVerb.new("yapmak").verb_stem).to eq("yap")
       expect(TurkishVerb.new("gitmek").verb_stem).to eq("git")
     end
