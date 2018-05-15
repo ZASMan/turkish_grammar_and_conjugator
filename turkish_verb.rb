2097a31fa12568663a85a112938cad4c4b2f0286
Zane Sandin <i.zane.am.robot@gmail.com>
2018-05-14 21:14:09 -0400

    Adds a conjugator for definite past tense

--- turkish_verb.rb
+++ turkish_verb.rb
@@ -3,22 +3,28 @@ require 'pry'
 
 class TurkishVerb
   include AllTurkishGrammarHelpers
+  include VerbConjugations
 
   attr_accessor :verb, :verb_stem
 
   def initialize(verb_infinitive)
     @verb = verb_infinitive.downcase
+    @word_without_consonants = @verb.delete(CONSONANT_LIST.join(''))
+    @word_without_vowels = @verb.delete(VOWEL_LIST.join(''))
     @verb_stem = @verb[0..-4]
+    @last_vowel = @word_without_consonants[-1]
+    @last_consonant = @word_without_vowels[-1]
+    @last_letter = @verb_stem.split("").last
   end
 
   def definite_past_tense
     {
-      ben: "",
-      sen: "",
-      o: "",
-      siz: "",
-      biz: "",
-      onlar: ""
+      ben: @verb_stem + definite_past_ending("ben"),
+      sen: @verb_stem + definite_past_ending("sen"),
+      o: @verb_stem + definite_past_ending("o"),
+      siz: @verb_stem + definite_past_ending("siz"),
+      biz: @verb_stem + definite_past_ending("biz"),
+      onlar: @verb_stem + definite_past_ending("onlar")
     }
   end
 end
